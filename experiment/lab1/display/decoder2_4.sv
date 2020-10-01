869c74a59b9ab194cc350dc4f5ab00a05fb0b821
15711657500 <15711657500@163.com>
2020-10-02 07:17:21 +0800

    update display

---
+++ experiment/lab1/display/decoder2_4.sv
@@ -0,0 +1,7 @@
+module decoder2_4 (
+    input logic [1:0] in,
+    output logic [3:0] out,
+    input logic en
+);
+    
+endmodule
---
+++ experiment/lab1/display/encoder4_2.sv
@@ -0,0 +1,6 @@
+module encoder4_2 (
+    input logic [3:0] in,
+    output logic [1:0] out
+);
+    
+endmodule
