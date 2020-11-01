a1aee6ae6340ba5c880a9599244c0feb52593562
15711657500 <15711657500@163.com>
2020-11-01 11:03:05 +0800

    start lab4

---
+++ experiment/lab4/ans.sv
@@ -0,0 +1,14 @@
+module ans (
+    input logic [3:0]in,
+    output logic [6:0]out
+);
+    always_comb begin
+        unique case(in)
+            
+            default: begin
+                
+            end
+        endcase
+    end
+    
+endmodule
---
+++ experiment/lab4/lab4.sv
@@ -0,0 +1,42 @@
+module lab4 (
+    input logic [3:0]in,
+    output logic [6:0]out_use138, out_use151
+);
+
+endmodule
+
+module seg_138 (
+    input logic [3:0]in,
+    output logic [6:0]out
+);
+    // TODO: add logic here
+    // NOTE: "always_comb" are not allowed here. You should use mod_74LS138 instants and logic gates(assign) here
+
+endmodule
+
+module mod_74LS138 (
+    // TODO: add ports here
+
+);
+    // TODO: add logic here
+    // NOTE: No syntax limitation
+
+endmodule
+
+module seg_151 (
+    input logic [3:0]in,
+    output logic [6:0]out
+);
+    // TODO: add logic here
+    // NOTE: "always_comb" are not allowed here. You should use mod_74LS151 instants and logic gates(assign) here
+
+endmodule
+
+module mod_74LS151 (
+    // TODO: add ports here
+    
+);
+    // TODO: add logic here
+    // NOTE: No syntax limitation
+
+endmodule
