cc18fa08f01ab7eff693e185405215ad590d3856
15711657500 <15711657500@163.com>
2020-11-02 09:12:15 +0800

    update doc

--- experiment/lab4/sim.sv
+++ experiment/lab4/sim.sv
@@ -17,7 +17,7 @@ module sim (
         end
     end
     logic [3:0]in;
-    logic [6:0]out_138, out_151, out_ans;
+    logic [6:0]out_use138, out_use151, out_ans;
     assign in = counter[3:0];
 
     lab4 lab4_inst(.*);
@@ -25,7 +25,7 @@ module sim (
 
     always_comb begin
         counter_nxt = counter;
-        if ((counter != 5'h10) && (out_138 == out_ans) && (out_151 == out_ans)) begin
+        if ((counter != 5'h10) && (out_use138 == out_ans) && (out_use151 == out_ans)) begin
             counter_nxt = counter_nxt + 1;
         end
     end
@@ -33,14 +33,14 @@ module sim (
     always_ff @(posedge clk) begin
         if (~resetn) begin
             
-        end else if (counter == 5'h10`) begin
+        end else if (counter == 5'h10) begin
             $display("PASS!\n");
             $finish;
-        end else if (~(out_138 === out_ans)) begin
-            $display("[%d ns]: Seg_138 (input 0x%x) expected 0x%x, got 0x%x!\n", $time, in, out_ans, out_138);
+        end else if (~(out_use138 === out_ans)) begin
+            $display("[%d ns]: Seg_138 (input 0x%x) expected 0x%x, got 0x%x!\n", $time, in, out_ans, out_use138);
             $finish;
-        end else if (~(out_151 === out_ans)) begin
-            $display("[%d ns]: Seg_151 (input 0x%x) expected 0x%x, got 0x%x!\n", $time, in, out_ans, out_151);
+        end else if (~(out_use151 === out_ans)) begin
+            $display("[%d ns]: Seg_151 (input 0x%x) expected 0x%x, got 0x%x!\n", $time, in, out_ans, out_use151);
             $finish;
         end
     end
--- experiment/lab4/top.sv
+++ experiment/lab4/top.sv
@@ -5,7 +5,7 @@ module top (
     output logic [6:0]A2G
 );
     
-    logic [10:0] counter;
+    logic [20:0] counter;
     always_ff @(posedge clk) begin
         if (~resetn) begin
             counter <= '0;
@@ -15,15 +15,16 @@ module top (
     end
     
     logic [15:0][6:0] outs;
+    logic [3:0] temp;
+    assign temp = {SW[15], counter[15:13]};
+    assign AN = ~(SW[7:0] & (8'h1 << counter[15:13]));
+    assign A2G = outs[temp];
 
-    assign AN = SW[7:0] & (8'h1 << counter[8:6]);
-    assign A2G = outs[{SW[15], counter[8:6]}];
-
-    for (genvar i = 0; i < 15; i++) begin
+    for (genvar i = 0; i < 16; i++) begin
         if ( i[0] ) begin
-            lab4 lab4_inst1(.in(i[2:0]), .out_138(outs[i]));
+            lab4 lab4_inst1(.in(i[3:0]), .out_use138(outs[i[3:0]]));
         end else begin
-            lab4 lab4_inst2(.in(i[2:0]), .out_151(outs[i]));
+            lab4 lab4_inst2(.in(i[3:0]), .out_use151(outs[i[3:0]]));
         end
     end
     
